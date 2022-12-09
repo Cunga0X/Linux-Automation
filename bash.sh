d8ba732c403d4b3d9d174c82c6f399b75c51c78e
Chunga <73850141+Cunga0X@users.noreply.github.com>
2022-12-09 14:17:34 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,13 +2,23 @@
 
 for i in "Documents" "Downloads" "Pictures" "Videos" "Docker"
     do
-        $ cd users
-        $ cd ~"$USER"
         $ mkdir $i || echo "Directory $i created successfuly."
     
     done
 
-echo "Task completed."
+echo "Dir taks successfuly finished"
+
+
+username=$(cat /usersinfo/users.txt | tr 'A-Z'  'a-z')
+
+password=$username@123
+
+for user in $username
+do
+       useradd $user
+       useradd $user sudo
+       echo $password | passwd --stdin $user
+done
 
 
 
