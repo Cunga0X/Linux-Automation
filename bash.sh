21c814d384f24d79824fb23fc674c07e1490cdf0
Chunga <73850141+Cunga0X@users.noreply.github.com>
2022-12-09 14:26:37 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -7,16 +7,13 @@ for i in "Documents" "Downloads" "Pictures" "Videos" "Docker"
 done
 
 
-username=$(cat /usersinfo/users | tr 'A-Z'  'a-z')
+username=$(cat /tmp/userlist | tr 'A-Z'  'a-z')
 
 password=$username@123
 
 for user in $username
 do
        useradd $user
-       useradd $user sudo
        echo $password | passwd --stdin $user
 done
-
-echo "$(wc -l /usersinfo/users) users have been created" 
 
