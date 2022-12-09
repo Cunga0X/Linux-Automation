5a68d192e0f7d30a6cfaa984c1daa7980eaaf27c
Chunga <73850141+Cunga0X@users.noreply.github.com>
2022-12-09 15:59:53 +0100

    install docker

--- install-docker.sh
+++ install-docker.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+
+for i in "Documents" "Downloads" "Pictures" "Videos" "Docker"
+    do
+        mkdir $i && echo "Directory $i created successfuly."
+    
+done
+
+
+username=$(cat users.txt | tr 'A-Z'  'a-z')
+
+password=$username@123
+
+for user in $username
+do
+       sudo useradd $user
+       $password
+done
+
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
